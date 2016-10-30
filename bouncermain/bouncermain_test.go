b861ff16eb9a0908c6ca7dd4f1fe77c01517b5bb
Pedro Werneck <pjwerneck@gmail.com>
2016-10-30 02:01:31 -0200

    Add more tests

--- bouncermain/bouncermain_test.go
+++ bouncermain/bouncermain_test.go
@@ -7,8 +7,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/pjwerneck/bouncer/bouncermain"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -42,44 +44,75 @@ func GetRequest(url string) (status int, body string, err error) {
 	return
 }
 
-func TestGetToken(t *testing.T) {
-	url := fmt.Sprintf("%s/v1/tokenbucket/testingbucket/acquire?size=10", server.URL)
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		t.Error(err)
+func TestGetTokensUntilEmpty(t *testing.T) {
+	url := fmt.Sprintf("%s/v1/tokenbucket/test1/acquire?size=100&maxwait=1", server.URL)
+	n := 0
+	for {
+		status, _, err := GetRequest(url)
+		require.Nil(t, err)
+		if status != 204 {
+			break
+		}
+		n++
 	}
+	require.Equal(t, 100, n)
+}
 
-	rep, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Error(err)
-	}
+func TestGetTokensUntilEmptyAndWaitForRefill(t *testing.T) {
+	url := fmt.Sprintf("%s/v1/tokenbucket/test2/acquire?size=10&maxwait=1&interval=10", server.URL)
 
-	if rep.StatusCode != 204 {
-		t.Errorf("204 No Content expected: %d", rep.StatusCode)
+	n := 0
+	for {
+		status, _, err := GetRequest(url)
+		require.Nil(t, err)
+		if status != 204 {
+			break
+		}
+		n++
 	}
+	require.Equal(t, 10, n)
+
+	time.Sleep(time.Duration(10 * 1e6))
+	for {
+		status, _, err := GetRequest(url)
+		require.Nil(t, err)
+		if status != 204 {
+			break
+		}
+		n++
+	}
+	require.Equal(t, 20, n)
 
 }
 
-func TestGetTokens(t *testing.T) {
-	url := fmt.Sprintf("%s/v1/tokenbucket/testingbucket/acquire?size=10&maxwait=1", server.URL)
+func TestSemaphoreAcquireAndRelease(t *testing.T) {
+	url := fmt.Sprintf("%s/v1/semaphore/test1/acquire?maxwait=1", server.URL)
 
-	for i := 0; i < 9; i++ {
-		status, body, err := GetRequest(url)
-		if err != nil {
-			t.Error(err)
-		}
-		if status != 204 {
-			t.Errorf("204 No Content expected: %v %v", status, body)
-		}
-	}
+	status, key, err := GetRequest(url)
+	require.Nil(t, err)
+	require.Equal(t, 200, status)
 
-	status, body, err := GetRequest(url)
-	if err != nil {
-		t.Error(err)
-	}
-	if status != 408 {
-		t.Errorf("408 Request Timeout expected: %v %v", status, body)
-	}
+	status, _, err = GetRequest(url)
+	require.Nil(t, err)
+	require.Equal(t, 408, status)
 
+	url = fmt.Sprintf("%s/v1/semaphore/test1/release?maxwait=1&key=%s", server.URL, key)
+	status, key, err = GetRequest(url)
+	require.Nil(t, err)
+	require.Equal(t, 204, status)
+
+}
+
+func TestEventWaitAndSend(t *testing.T) {
+	time.AfterFunc(time.Duration(1e7),
+		func() {
+
+			status, _, err := GetRequest(fmt.Sprintf("%s/v1/event/test1/send", server.URL))
+			require.Nil(t, err)
+			require.Equal(t, 204, status)
+		})
+
+	status, _, err := GetRequest(fmt.Sprintf("%s/v1/event/test1/wait?maxwait=100", server.URL))
+	require.Nil(t, err)
+	require.Equal(t, 204, status)
 }
